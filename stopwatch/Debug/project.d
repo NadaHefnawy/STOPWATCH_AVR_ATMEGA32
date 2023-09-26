ea92d85079b67441e676a8d4a48c4e1bfe0d51cb
Nada Hefnawy <nadaelhefnawy2002@gmail.com>
2023-09-26 17:32:21 +0300

    stopwatch

---
+++ stopwatch/Debug/project.d
@@ -0,0 +1,5 @@
+project.o project.o: ../project.c ../timer.h ../std_types.h
+
+../timer.h:
+
+../std_types.h:
---
+++ stopwatch/Debug/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../gpio.c \
+../project.c \
+../timer.c 
+
+OBJS += \
+./gpio.o \
+./project.o \
+./timer.o 
+
+C_DEPS += \
+./gpio.d \
+./project.d \
+./timer.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -Wall -g2 -gstabs -O0 -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega32 -DF_CPU=1000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
